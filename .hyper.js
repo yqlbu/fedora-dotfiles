ccdc4751173caf5fba0f9c25513578846f3d417a
yqlbu <yqlbu@bu.edu>
2023-03-17 00:23:25 +0800

    update all

--- .hyper.js
+++ .hyper.js
@@ -124,7 +124,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-snazzy'],
+  plugins: ['hyper-snazzy', "hyper-tab-icons", "hyper-opacity"],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
